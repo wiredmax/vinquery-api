66349bb98b86595647d3a27b5b4abe9cc3b3ebac
Maxime Poulin <mpoulin@roux.ca>
2013-09-12 11:52:36 -0400

    Supports only lite reports for now.

--- controllers/api.js
+++ controllers/api.js
@@ -41,29 +41,13 @@ exports.check = (function(req, res) {
                   else {
                     var vin = new Vin({ 
                       vin: result.VINquery.VIN[0].$.Number, 
-                      year: result.VINquery.VIN[0].Vehicle[0].Item[1].$.Value, 
-                      make: result.VINquery.VIN[0].Vehicle[0].Item[2].$.Value, 
-                      model: result.VINquery.VIN[0].Vehicle[0].Item[3].$.Value, 
-                      trim_level: result.VINquery.VIN[0].Vehicle[0].Item[4].$.Value, 
-                      manufactured_in: result.VINquery.VIN[0].Vehicle[0].Item[5].$.Value, 
-                      body_style: result.VINquery.VIN[0].Vehicle[0].Item[7].$.Valuee, 
-                      engine_type: result.VINquery.VIN[0].Vehicle[0].Item[8].$.Value, 
-                      transmission_long: result.VINquery.VIN[0].Vehicle[0].Item[9].$.Value, 
-                      transmission_short: result.VINquery.VIN[0].Vehicle[0].Item[10].$.Value, 
-                      driveline: result.VINquery.VIN[0].Vehicle[0].Item[11].$.Value, 
-                      tank: result.VINquery.VIN[0].Vehicle[0].Item[12].$.Value, 
-                      tank_unit: result.VINquery.VIN[0].Vehicle[0].Item[12].$.Unit,
-                      fuel_economy_city: result.VINquery.VIN[0].Vehicle[0].Item[13].$.Value, 
-                      fuel_economy_highway: result.VINquery.VIN[0].Vehicle[0].Item[14].$.Value, 
-                      fuel_economy_unit: result.VINquery.VIN[0].Vehicle[0].Item[14].$.Unit, 
-                      anti_brake_system: result.VINquery.VIN[0].Vehicle[0].Item[15].$.Value, 
-                      steering_type: result.VINquery.VIN[0].Vehicle[0].Item[16].$.Value, 
-                      standard_seating: result.VINquery.VIN[0].Vehicle[0].Item[17].$.Value, 
-                      optional_seating: result.VINquery.VIN[0].Vehicle[0].Item[18].$.Value, 
-                      length: result.VINquery.VIN[0].Vehicle[0].Item[19].$.Value, 
-                      width: result.VINquery.VIN[0].Vehicle[0].Item[20].$.Value, 
-                      height: result.VINquery.VIN[0].Vehicle[0].Item[21].$.Value, 
-                      dimension_unit: result.VINquery.VIN[0].Vehicle[0].Item[21].$.Unit 
+                      year: result.VINquery.VIN[0].Vehicle[0].Item[0].$.Value, 
+                      make: result.VINquery.VIN[0].Vehicle[0].Item[1].$.Value, 
+                      model: result.VINquery.VIN[0].Vehicle[0].Item[2].$.Value, 
+                      trim_level: result.VINquery.VIN[0].Vehicle[0].Item[3].$.Value, 
+                      manufactured_in: result.VINquery.VIN[0].Vehicle[0].Item[4].$.Value, 
+                      body_style: result.VINquery.VIN[0].Vehicle[0].Item[5].$.Value, 
+                      engine_type: result.VINquery.VIN[0].Vehicle[0].Item[6].$.Value
                     });
                       
                     vin.save(function (err) {
